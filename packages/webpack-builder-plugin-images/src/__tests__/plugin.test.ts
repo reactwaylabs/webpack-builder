7c9592395477e719580bca13fd3ba37eedba2a0a
Eimantas Dumse <eimantas@quatrodev.com>
2019-05-17 16:27:34 +0300

    Faked path.

--- packages/webpack-builder-plugin-images/src/__tests__/plugin.test.ts
+++ packages/webpack-builder-plugin-images/src/__tests__/plugin.test.ts
@@ -1,11 +1,14 @@
-jest.mock("@reactway/image-loader");
+import { ReactwayImageLoaderPlugin } from "@reactway/image-loader";
 __dirname = "TEST_PATH";
+ReactwayImageLoaderPlugin.loader = "./path/to/link";
 
 jest.mock("upath", () => ({
     ...jest.requireActual("upath"),
     resolve: (...pathSegments: string[]) => pathSegments.join("//"),
     join: (...pathSegments: string[]) => pathSegments.join("//")
 }));
+
+jest.mock("@reactway/image-loader");
 
 import { Builder, Configuration } from "@reactway/webpack-builder";
 import upath from "upath";
