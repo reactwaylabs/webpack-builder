1e02a649f838f7651cc83b58d02d07ec0dec7573
Eimantas <eimantas@quatrodev.com>
2019-05-31 15:49:04 +0300

    Hotfix: Postcss plugin (#15)
    * WIP.
    
    * Updates.
    
    * Applying package updates.
    
    * Versions update.
    
    * Removed commented code.

--- packages/webpack-builder-plugin-styles/src/__tests__/plugin.test.ts
+++ packages/webpack-builder-plugin-styles/src/__tests__/plugin.test.ts
@@ -18,6 +18,49 @@ const path = "./src/__tests__/";
 let SAMPLE_CONFIGURATION: Configuration = {};
 const TEST_PROJECT_LOCATION: string = upath.resolve(path, "./test-project");
 
+describe("production", () => {
+    beforeEach(() => {
+        SAMPLE_CONFIGURATION = {
+            entry: "./src/index.ts",
+            mode: "production",
+            output: {
+                path: upath.resolve(TEST_PROJECT_LOCATION, "dist"),
+                filename: "[name].bundle.js",
+                chunkFilename: "[name].bundle.js",
+                publicPath: "./"
+            }
+        };
+    });
+
+    it("Adding styles plugin to configuration in production mode", () => {
+        const configuration = new Builder(TEST_PROJECT_LOCATION, SAMPLE_CONFIGURATION).use(StylesPlugin).toConfig();
+        expect(configuration).toMatchSnapshot();
+    });
+
+    it("Adding styles plugin to configuration with optimization exist in production mode", () => {
+        SAMPLE_CONFIGURATION = {
+            ...SAMPLE_CONFIGURATION,
+            optimization: {
+                minimizer: []
+            }
+        };
+        const configuration = new Builder(TEST_PROJECT_LOCATION, SAMPLE_CONFIGURATION).use(StylesPlugin).toConfig();
+        expect(configuration).toMatchSnapshot();
+    });
+
+    it("Adding styles plugin to configuration with mini css options", () => {
+        const configuration = new Builder(TEST_PROJECT_LOCATION, SAMPLE_CONFIGURATION)
+            .use(StylesPlugin, {
+                miniCssExtractPluginOptions: {
+                    filename: "[name].[chunkhash].css",
+                    chunkFilename: "[name].[contenthash].css"
+                }
+            })
+            .toConfig();
+        expect(configuration).toMatchSnapshot();
+    });
+});
+
 describe("development", () => {
     beforeEach(() => {
         SAMPLE_CONFIGURATION = {
@@ -33,6 +76,17 @@ describe("development", () => {
     });
 
     it("Adding styles plugin to configuration", () => {
+        const configuration = new Builder(TEST_PROJECT_LOCATION, SAMPLE_CONFIGURATION).use(StylesPlugin).toConfig();
+        expect(configuration).toMatchSnapshot();
+    });
+
+    it("Adding styles plugin with resolve and extensions exists to configuration", () => {
+        SAMPLE_CONFIGURATION = {
+            ...SAMPLE_CONFIGURATION,
+            resolve: {
+                extensions: [".css", ".scss"]
+            }
+        };
         const configuration = new Builder(TEST_PROJECT_LOCATION, SAMPLE_CONFIGURATION).use(StylesPlugin).toConfig();
         expect(configuration).toMatchSnapshot();
     });
@@ -59,6 +113,16 @@ describe("development", () => {
                     options: {
                         limit: 8192
                     }
+                }
+            })
+            .toConfig();
+        expect(configuration).toMatchSnapshot();
+    });
+    it("Adding styles plugin with url options as string to configuration", () => {
+        const configuration = new Builder(TEST_PROJECT_LOCATION, SAMPLE_CONFIGURATION)
+            .use(StylesPlugin, {
+                urlLoaderOptions: {
+                    options: "?limit=8192"
                 }
             })
             .toConfig();
@@ -117,42 +181,66 @@ describe("development", () => {
         expect(configuration).toMatchSnapshot();
     });
 
-    it("PostCss config do not exist", () => {
+    it("PostCss config do not exist and warn user", () => {
         const projectLocation = upath.resolve(path, "./postcss-config-not-exist");
         fs.emptyDir(projectLocation);
+        const spy = jest.spyOn(global.console, "warn");
         const configuration = new Builder(projectLocation, SAMPLE_CONFIGURATION).use(StylesPlugin).toConfig();
-        expect(configuration).toMatchSnapshot();
-    });
-});
-
-describe("production", () => {
-    beforeEach(() => {
-        SAMPLE_CONFIGURATION = {
-            entry: "./src/index.ts",
-            mode: "production",
-            output: {
-                path: upath.resolve(TEST_PROJECT_LOCATION, "dist"),
-                filename: "[name].bundle.js",
-                chunkFilename: "[name].bundle.js",
-                publicPath: "./"
-            }
-        };
-    });
-
-    it("Adding styles plugin to configuration in production mode", () => {
-        const configuration = new Builder(TEST_PROJECT_LOCATION, SAMPLE_CONFIGURATION).use(StylesPlugin).toConfig();
+        expect(spy).toHaveBeenCalled();
         expect(configuration).toMatchSnapshot();
     });
 
-    it("Adding styles plugin to configuration with mini css options", () => {
+    it("PostCss config with an object options", () => {
         const configuration = new Builder(TEST_PROJECT_LOCATION, SAMPLE_CONFIGURATION)
             .use(StylesPlugin, {
-                miniCssExtractPluginOptions: {
-                    filename: "[name].[chunkhash].css",
-                    chunkFilename: "[name].[contenthash].css"
+                postcssLoaderOptions: {
+                    options: {
+                        sourceMap: "inline"
+                    }
                 }
             })
             .toConfig();
+
+        expect(configuration).toMatchSnapshot();
+    });
+
+    it("PostCss config with a query string options", () => {
+        const configuration = new Builder(TEST_PROJECT_LOCATION, SAMPLE_CONFIGURATION)
+            .use(StylesPlugin, {
+                postcssLoaderOptions: {
+                    options: "?sourceMap=inline"
+                }
+            })
+            .toConfig();
+
+        expect(configuration).toMatchSnapshot();
+    });
+
+    it("PostCss config with a plugin array", () => {
+        const configuration = new Builder(TEST_PROJECT_LOCATION, SAMPLE_CONFIGURATION)
+            .use(StylesPlugin, {
+                postcssLoaderOptions: {
+                    options: {
+                        plugins: [require("autoprefixer")()]
+                    }
+                }
+            })
+            .toConfig();
+
+        expect(configuration).toMatchSnapshot();
+    });
+
+    it("PostCss config with a plugin function", () => {
+        const configuration = new Builder(TEST_PROJECT_LOCATION, SAMPLE_CONFIGURATION)
+            .use(StylesPlugin, {
+                postcssLoaderOptions: {
+                    options: {
+                        plugins: (options: unknown) => [require("autoprefixer")({ ...options })]
+                    }
+                }
+            })
+            .toConfig();
+
         expect(configuration).toMatchSnapshot();
     });
 });
