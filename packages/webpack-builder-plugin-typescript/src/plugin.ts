25b70c060a8b0ccd161e0c9363bf02ba12648c1e
Eimantas <eimantas@quatrodev.com>
2019-06-05 12:41:50 +0300

    Hotfix: babel warning (#17)
    * Added `compact`  to `jsx`  loader.
    
    * Applying package updates.
    
    * Updated versions.

--- packages/webpack-builder-plugin-typescript/src/plugin.ts
+++ packages/webpack-builder-plugin-typescript/src/plugin.ts
@@ -104,7 +104,8 @@ export const TypeScriptPlugin: Plugin<TypeScriptPluginOptions> = (config, projec
                 {
                     loader: "babel-loader",
                     options: {
-                        presets: ["@babel/preset-env"]
+                        presets: ["@babel/preset-env"],
+                        compact: true
                     }
                 }
             ],
